30e86e6afdb74ce499188d36c1fc66a76d62a36d
oh <wish.master.mvp@gmail.com>
2025-01-17 00:16:28 +0200

    Handle a potential division by zero runtime error

--- app/main.py
+++ app/main.py
@@ -24,6 +24,9 @@ class CarWashStation:
         return round(total_income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
+        if self.distance_from_city_center == 0:
+            raise ValueError("Distance from city center must be greater than zero.")
+
         return round(car.comfort_class
                      * (self.clean_power - car.clean_mark)
                      * self.average_rating
