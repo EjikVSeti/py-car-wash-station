ae3450e741b40f17d1b09de007a376ca5f4f0e21
oh <wish.master.mvp@gmail.com>
2025-01-17 00:19:05 +0200

    Fix flake8 formatting

--- app/main.py
+++ app/main.py
@@ -25,7 +25,9 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         if self.distance_from_city_center == 0:
-            raise ValueError("Distance from city center must be greater than zero.")
+            raise ValueError(
+                "Distance from city center must be greater than zero."
+            )
 
         return round(car.comfort_class
                      * (self.clean_power - car.clean_mark)
