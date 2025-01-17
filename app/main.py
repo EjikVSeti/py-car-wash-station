9d9260d1ef5c1d809112f20f036a9fe4dd121935
oh <wish.master.mvp@gmail.com>
2025-01-17 15:19:30 +0200

    Revert "Fix another way zero division error"
    This reverts commit 838a44c723548e0286143ecc3f5e21c502cd974e.

--- app/main.py
+++ app/main.py
@@ -24,16 +24,16 @@ class CarWashStation:
         return round(total_income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        try:
-            return round(
-                car.comfort_class
-                * (self.clean_power - car.clean_mark)
-                * self.average_rating
-                / self.distance_from_city_center,
-                1
+        if self.distance_from_city_center == 0:
+            raise ValueError(
+                "Distance from city center must be greater than zero."
             )
-        except ZeroDivisionError:
-            return 0
+
+        return round(car.comfort_class
+                     * (self.clean_power - car.clean_mark)
+                     * self.average_rating
+                     / self.distance_from_city_center
+                     , 1)
 
     def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
